bbfdc37467998b20f842046cf90c8595f3d4a2b8
Floorban <15696082812@163.com>
2023-12-05 15:30:50 +0100

    initial

---
+++ Assets/FPS/Scripts/Gameplay/ArmorPickup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.FPS.Gameplay;
+
+namespace Assets.FPS.Scripts.Gameplay
+{
+  public  class ArmorPickup : Pickup
+  {
+    protected override void OnPicked(PlayerCharacterController playerController)
+    {
+      base.OnPicked(playerController);
+
+      // Do things with Armor here.
+    }
+  }
+}
