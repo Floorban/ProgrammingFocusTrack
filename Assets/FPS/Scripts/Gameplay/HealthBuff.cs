97c2bc63885a8165b81a333a60cda014d22f9622
Floorban <15696082812@163.com>
2023-12-08 16:12:54 +0100

    HealthPowerups

---
+++ Assets/FPS/Scripts/Gameplay/HealthBuff.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.FPS.Game;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PowerUps/HealthBuff")]
+public class HealthBuff : PowerUpEffect
+{
+    public float healAmount;
+    public override void Apply(GameObject player)
+    {
+        player.GetComponent<Health>().CurrentHealth += healAmount;
+    }
+}
--- Assets/FPS/Scripts/Gameplay/HealthPickup.cs
+++ Assets/FPS/Scripts/Gameplay/HealthPickup.cs
@@ -5,18 +5,21 @@ namespace Unity.FPS.Gameplay
 {
     public class HealthPickup : Pickup
     {
-        [Header("Parameters")] [Tooltip("Amount of health to heal on pickup")]
-        public float HealAmount;
+        //[Header("Parameters")] [Tooltip("Amount of health to heal on pickup")]
+        //public float HealAmount;
 
+        public PowerUpEffect powerUpEffect;
         protected override void OnPicked(PlayerCharacterController player)
         {
             Health playerHealth = player.GetComponent<Health>();
             if (playerHealth && playerHealth.CanPickup())
             {
-                playerHealth.Heal(HealAmount);
+                //playerHealth.Heal(HealAmount);
+                powerUpEffect.Apply(player.gameObject);
                 PlayPickupFeedback();
                 Destroy(gameObject);
             }
         }
+
     }
 }
