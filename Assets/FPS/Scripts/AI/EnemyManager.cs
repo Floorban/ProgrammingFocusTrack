aab22624512caeb18821e689868f4b2c90ab5cb7
Floorban <15696082812@163.com>
2023-12-10 00:35:22 +0100

    enemy spawner

--- Assets/FPS/Scripts/AI/EnemyManager.cs
+++ Assets/FPS/Scripts/AI/EnemyManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Unity.FPS.Game;
+using Unity.FPS.Gameplay;
 using UnityEngine;
 
 namespace Unity.FPS.AI
@@ -10,11 +11,34 @@ namespace Unity.FPS.AI
         public int NumberOfEnemiesTotal { get; private set; }
         public int NumberOfEnemiesRemaining => Enemies.Count;
 
+        [SerializeField] GameObject enemyPrefabs;
+        [SerializeField] Vector3 spawnArea;
+        [SerializeField] float spawnTimer;
+        private float timer;
+        public GameObject player;
+
         void Awake()
         {
             Enemies = new List<EnemyController>();
         }
+        private void Update()
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                SpawnEnemy();
+                timer = spawnTimer;
+            }
+        }
+        void SpawnEnemy()
+        {
+            Vector3 position = new Vector3(Random.Range(-spawnArea.x, spawnArea.x), 0, Random.Range(-spawnArea.z, spawnArea.z));
 
+            position += player.transform.position;
+
+            GameObject newEnemy = Instantiate(enemyPrefabs);
+            newEnemy.transform.position = position;
+        }
         public void RegisterEnemy(EnemyController enemy)
         {
             Enemies.Add(enemy);
