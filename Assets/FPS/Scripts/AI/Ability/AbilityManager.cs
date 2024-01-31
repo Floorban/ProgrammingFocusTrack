79bfd8fbb054c93a3e18cd00cc1b76ca819cd940
Floorban <15696082812@163.com>
2024-01-31 13:43:17 +0100

    ability panel set uup

--- Assets/FPS/Scripts/AI/Ability/AbilityManager.cs
+++ Assets/FPS/Scripts/AI/Ability/AbilityManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Unity.FPS.AI
 {
@@ -15,48 +16,75 @@ namespace Unity.FPS.AI
             cooldown
         }
         [SerializeField] State[] states;
+        [SerializeField] bool[] canUsed;
+        [SerializeField] Button[] buttons;
+        [SerializeField] GameObject abilityPanel;
         void Start()
         {
+            abilityPanel.SetActive(false);
             activeTimes = new float[abilities.Length];
             cooldownTimes = new float[abilities.Length];
             states = new State[abilities.Length];
+
+            canUsed = new bool[3];
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].onClick.AddListener(EnableAbility);
+            }
+
         }
         void Update()
         {
+            HandleAbilityStateSwitch();
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                  abilityPanel.SetActive(!abilityPanel.activeSelf);
+            }
+        }
+        public void EnableAbility()
+        {
+            for (int i = 0; i < canUsed.Length; i++)
+            {
+                canUsed[i] = true;
+            }
+        }
+        void HandleAbilityStateSwitch()
+        {
             for (int i = 0; i < abilities.Length; i++)
             {
-                switch (states[i])
-                {
-                    case State.ready:
-                        if (Input.GetKeyDown(abilityKeys[i]))
-                        {
-                            abilities[i].Activate(gameObject);
-                            states[i] = State.activated;
-                            activeTimes[i] = abilities[i].activeTime;
-                        }
-                        break;
-                    case State.activated:
-                        if (activeTimes[i] > 0)
-                        {
-                            activeTimes[i] -= Time.deltaTime;
-                        }
-                        else
-                        {
-                            states[i] = State.cooldown;
-                            cooldownTimes[i] = abilities[i].cooldownTime;
-                        }
-                        break;
-                    case State.cooldown:
-                        if (cooldownTimes[i] > 0)
-                        {
-                            cooldownTimes[i] -= Time.deltaTime;
-                        }
-                        else
-                        {
-                            states[i] = State.ready;
-                        }
-                        break;
-                }
+                if (!canUsed[i]) continue;
+                    switch (states[i])
+                    {
+                        case State.ready:
+                            if (Input.GetKeyDown(abilityKeys[i]))
+                            {
+                                abilities[i].Activate(gameObject);
+                                states[i] = State.activated;
+                                activeTimes[i] = abilities[i].activeTime;
+                            }
+                            break;
+                        case State.activated:
+                            if (activeTimes[i] > 0)
+                            {
+                                activeTimes[i] -= Time.deltaTime;
+                            }
+                            else
+                            {
+                                states[i] = State.cooldown;
+                                cooldownTimes[i] = abilities[i].cooldownTime;
+                            }
+                            break;
+                        case State.cooldown:
+                            if (cooldownTimes[i] > 0)
+                            {
+                                cooldownTimes[i] -= Time.deltaTime;
+                            }
+                            else
+                            {
+                                states[i] = State.ready;
+                            }
+                            break;
+                    }           
             }
         }
     }
